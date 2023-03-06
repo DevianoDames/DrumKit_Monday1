0b16823509b6133dea336bf52433320527d202a6
DevianoDames <d_dames183654@fanshaweonline.ca>
2023-03-06 16:00:59 -0500

    added code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 // drum kit: press a key to play the sound. use JavaScript to retrieve the data-key attribute and then use that as a selector to find the matching audio file and play it!
-
-(() => {
     console.log('music player script file');
+
+    // listen for the keyboard events happening in the browser window
+    window.addEventListener('keyup', findMatchingAudio);
+
+    function findMatchingAudio(event) {
+        debugger;
+    }
     
-})();
